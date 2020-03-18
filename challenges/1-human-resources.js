32108d27a637a8b0ea96a9d85b1ebaf39ef93250
nax2uk <azlinayeo@gmail.com>
2020-03-18 09:42:36 +0000

    completed 1-human-resources---makeNameTags

--- challenges/1-human-resources.js
+++ challenges/1-human-resources.js
@@ -1,20 +1,17 @@
-// 1
-//const employees = [{ name: 'Sam', profession: 'artist' }, { name: 'Mitch', profession: 'mole' }];
-// returns [{name: 'Sam', profession: 'artist'}];
-
-//removeAgents(employees);
-
 function removeAgents(arrOfObjEmployees) {
   return arrOfObjEmployees.filter((objEmployees) => !(objEmployees.profession === 'mole'));
-  //return [{ name: 'Sam', profession: 'artist' }];
 }
 
 // 2
 
-function makeNameTags(guests) {}
+function makeNameTags(arrOfObjGuests) {
+  return arrOfObjGuests.map(
+    (objGuest) => `${objGuest.title} ${objGuest.forename} ${objGuest.surname}, ${objGuest.company}`
+  );
+}
 
 // 3
 
 function createPoll(items) {}
 
-module.exports = { removeAgents };
+module.exports = { removeAgents, makeNameTags };
--- spec/1-human-resources.spec.js
+++ spec/1-human-resources.spec.js
@@ -1,5 +1,5 @@
 const { expect } = require('chai');
-const { removeAgents } = require('../challenges/1-human-resources.js');
+const { removeAgents, makeNameTags } = require('../challenges/1-human-resources.js');
 
 describe('#removeAgents', () => {
   it('checks that function returns an array', () => {
@@ -51,5 +51,81 @@ describe('#removeAgents', () => {
       { name: 'Daisy', profession: 'mole' },
     ];
     expect(removeAgents(inputArray)).to.eql([]);
+  });
+});
+
+describe('#makeNameTags', () => {
+  it('checks that function returns an array', () => {
+    const inputArray = [
+      {
+        title: 'Mr',
+        forename: 'Sam',
+        surname: 'Caine',
+        age: 30,
+        company: 'Northcoders',
+      },
+    ];
+    expect(makeNameTags(inputArray)).to.be.an('array');
+  });
+
+  it('checks that function returns an empty array when passed an empty array', () => {
+    const inputArray = [];
+    expect(makeNameTags(inputArray)).to.eql([]);
+  });
+
+  it('checks that function returns a new array', () => {
+    const inputArray = [
+      {
+        title: 'Mr',
+        forename: 'Sam',
+        surname: 'Caine',
+        age: 30,
+        company: 'Northcoders',
+      },
+    ];
+    expect(makeNameTags(inputArray)).to.not.equal(inputArray);
+  });
+
+  it('checks that function return the correctly formatted array', () => {
+    inputArray = [
+      {
+        title: 'Mr',
+        forename: 'Sam',
+        surname: 'Caine',
+        age: 30,
+        company: 'Northcoders',
+      },
+    ];
+
+    expect(makeNameTags(inputArray)).to.eql(['Mr Sam Caine, Northcoders']);
+
+    inputArray = [
+      {
+        title: 'Mr',
+        forename: 'Sam',
+        surname: 'Caine',
+        age: 30,
+        company: 'Northcoders',
+      },
+      {
+        title: 'Mr',
+        forename: 'Beyazid',
+        surname: 'Kocak',
+        age: 12,
+        company: 'MGS',
+      },
+      {
+        title: 'Mrs',
+        forename: 'Samantha',
+        surname: 'Jones',
+        age: 22,
+        company: 'British Gas',
+      },
+    ];
+    expect(makeNameTags(inputArray)).to.eql([
+      'Mr Sam Caine, Northcoders',
+      'Mr Beyazid Kocak, MGS',
+      'Mrs Samantha Jones, British Gas',
+    ]);
   });
 });
